a68d71aae50cf49bb79a36ed0c8f3a8350cf41b0
Nicole Deflaux <nicole.deflaux@gmail.com>
2014-06-15 11:45:27 -0700

    A query to demonstrate that the allelic frequencies computed for PGP correspond to those from the 1,000 genomes project.

---
+++ pgp/sql/allelic-frequency-comparison.sql
@@ -0,0 +1,36 @@
+# PGP vs. 1,000 Genomes allelic frequency comparison
+SELECT
+  chromosome,
+  reference,
+  locusBegin,
+  locusEnd,
+  allele,
+  pgp_freq,
+  af,
+  eur_af,
+  afr_af,
+  asn_af,
+  amr_af
+FROM
+  [google.com:biggene:1000genomes.variants1kG] AS kg
+JOIN (
+  SELECT
+    chromosome,
+    REGEXP_EXTRACT(chromosome,
+      r'chr(\d+)') AS contig,
+    reference,
+    locusBegin + 1 AS position,
+    locusBegin,
+    locusEnd,
+    allele,
+    freq AS pgp_freq
+  FROM
+    [deflaux-test-1:pgp.brca1_freq_with_ins]
+    ) AS pgp
+ON
+  pgp.contig = kg.contig
+  AND pgp.position = kg.position
+  AND pgp.reference = kg.reference_bases
+  AND pgp.allele = kg.alternate_bases_str
+WHERE
+  kg.contig = '17'
