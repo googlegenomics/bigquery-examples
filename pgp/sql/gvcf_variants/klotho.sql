fcfceea095317cfd37c70be88bc98082e8fe1cff
Nicole Deflaux <nicole.deflaux@gmail.com>
2014-08-04 18:33:15 -0700

    Added klotho queries for two other tables.

---
+++ pgp/sql/gvcf_variants/klotho.sql
@@ -0,0 +1,35 @@
+# Sample level data for Klotho variant rs9536314 for use in the "amazing
+# intelligence of PGP participants" data story, specifically joining two 
+# tables to compare the different encodings.
+SELECT
+  contig_name,
+  start_pos,
+  end_pos,
+  END,
+  ref,
+  alt,
+  sample_id,
+  genotype
+FROM
+  FLATTEN(
+  SELECT
+    contig_name,
+    start_pos,
+    end_pos,
+    END,
+    reference_bases AS ref,
+    GROUP_CONCAT(alternate_bases) WITHIN RECORD AS alt,
+    call.callset_name AS sample_id,
+    GROUP_CONCAT(STRING(call.genotype),
+      '/') WITHIN call AS genotype,
+  FROM
+    [google.com:biggene:test.pgp_gvcf_variants]
+  WHERE
+    contig_name = '13'
+    AND start_pos <= 33628138
+    AND (end_pos >= 33628139
+      OR END >= 33628139)
+    ,
+    call)
+ORDER BY
+  sample_id
